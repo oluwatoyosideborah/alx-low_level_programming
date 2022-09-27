9b662fd958c95dbf634262d14f4b9c049ed4bdbb
oluwatoyosideborah <akomolehinoluwatoyosi@gmail.com>
2022-09-27 05:00:37 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
 /**
- * _memset - sets memory to array
- * @s: array to set
- * @b: value to set it as
- * @n: n amount of times
- * Return: char value of s
+ * _memset - check the code.
+ * @s: parameter for the array
+ * @b: parameter for the values in the array
+ * @n: parameter for the number of times to print the new values
+ * Return: Always 0.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a = 0;
 
-	for (i = 0; i < n; i++)
+	while (a < n)
 	{
-		s[i] = b;
+		s[a] = b;
+		a++;
 	}
+
 	return (s);
 }
